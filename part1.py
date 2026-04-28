d75f30f87e05df847ac8a15cb56a0b254d5f0c00
aakanksha06 <aakanksha.sah.3228@gmail.com>
2026-04-29 02:02:19 +0530

    Create part1.py

---
+++ part1.py
@@ -0,0 +1,36 @@
+@app.route('/api/products', methods=['POST'])
+def create_product():
+    data = request.json
+   
+    #Creating Required fields
+     required-[‘name’, ‘sku’, ‘price’, ‘warehouse_id’, ‘initial_quantity’]
+     for field in required:
+	if field not in data or data[field] is None:
+		return{“error”: f“Missing {field}”},400
+ 
+
+    # Checking if SKU exists
+     if Product.query.filter_by(sku=data[‘sku]).first():
+	return {“error”: “SKU already exists”} , 409
+
+    try:
+     #Saving both together
+      product = Product(
+		name=data['name'],
+        	sku=data['sku'],
+        	price=float(data['price']))
+    db.session.add(product)
+    db.session.flush()
+
+     inventory=Inventory(
+Product_id=product.id, 
+warehouse_id=data['warehouse_id'], 
+            quantity=data[‘initial_quantity’]
+    )
+    
+    db.session.add( inventory)
+    db.session.commit()    
+             return {"message": "Product created", "product_id": product.id}
+     except Exception as e:
+	db.session.rollback()
+	return{“error” : str(e)}, 500
